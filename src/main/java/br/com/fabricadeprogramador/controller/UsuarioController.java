0bd9341217b7b5b8cc20bbec43114ed182d1110c
virmerson <virmerson@hightechcursos.com.br>
2016-03-12 21:53:00 -0400

    Integrando JSF com Spring

--- src/main/java/br/com/fabricadeprogramador/controller/UsuarioController.java
+++ src/main/java/br/com/fabricadeprogramador/controller/UsuarioController.java
@@ -2,15 +2,31 @@ package br.com.fabricadeprogramador.controller;
 
 import javax.faces.bean.ManagedBean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import br.com.fabricadeprogramador.entidade.Usuario;
+import br.com.fabricadeprogramador.service.ServiceException;
+import br.com.fabricadeprogramador.service.UsuarioService;
 
 @ManagedBean(name="usuarioController")
+@Controller(value="usuarioController")
 public class UsuarioController {
 	private Usuario usuario= new Usuario();
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	
 	public void salvar(){
 		//
-		System.out.println("Salvando usuario:" + usuario.getNome() +" "+ usuario.getLogin() + " "+ usuario.getSenha() );
+		try {
+			usuarioService.salvar(usuario);
+			System.out.println("Salvando usuario:" + usuario.getNome() +" "+ usuario.getLogin() + " "+ usuario.getSenha() );
+			
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public Usuario getUsuario() {
